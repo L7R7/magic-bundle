b8c36753188f8be243ebd93a4703fba74ab49478
Leonhard Riedißer <leo008180@gmail.com>
2017-03-02 10:00:10 +0100

    Create module for consuming two services

---
+++ consumer-multi-service/src/main/java/de/l7r7/proto/bundle/magic/consumer/multiservice/Activator.java
@@ -0,0 +1,16 @@
+package de.l7r7.proto.bundle.magic.consumer.multiservice;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    @Override
+    public void start(BundleContext context) throws Exception {
+
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+
+    }
+}
